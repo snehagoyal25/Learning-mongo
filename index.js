a6efe2b59a9ed3a8756a0d2fb4b6f723e9a67975
snehagoyal25 <goyalsneha995@gmail.com>
2025-02-26 14:08:16 +0530

    learningg

---
+++ index.js
@@ -0,0 +1,18 @@
+const express =require("express");
+const app = express ();
+
+app.post("/signup",function(req,res){
+
+});
+
+app.post("/login",function(req,res){
+
+});
+
+app.post("/todo",function(req,res){
+
+});
+
+app.get("/todos",function(req,res){
+
+});
